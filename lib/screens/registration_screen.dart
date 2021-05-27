a0bf20344742cd619b4a5e8d0d6f5280b6ef5868
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2021-05-27 12:00:26 +0700

    registration screen

---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 200,
+              child: Image.asset("image/logo.png"),
+            ),
+            SizedBox(
+              height: 48,
+            ),
+            TextField(
+              onChanged: null,
+              decoration: InputDecoration(
+                hintText: "Enter your email",
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: Material(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(30),
+                ),
+                elevation: 5,
+                child: MaterialButton(
+                  onPressed: null,
+                  minWidth: 200,
+                  height: 42,
+                  child: Text(
+                    "Register",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
