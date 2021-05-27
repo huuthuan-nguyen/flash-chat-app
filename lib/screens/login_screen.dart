ee1d492220cf595eb8449ad4da86bbe286733b98
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2021-05-27 12:18:55 +0700

    Login Screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 200,
+              child: Image.asset("image/logo.png"),
+            ),
+            SizedBox(
+              height: 48,
+            ),
+            TextField(
+              onChanged: null,
+              decoration: InputDecoration(
+                hintText: "Enter your email",
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.lightBlueAccent,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.lightBlueAccent,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextField(
+              onChanged: null,
+              decoration: InputDecoration(
+                hintText: "Enter your password.",
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.lightBlueAccent, width: 1),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.lightBlueAccent, width: 2),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: Material(
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(30),
+                ),
+                elevation: 5,
+                child: MaterialButton(
+                  onPressed: null,
+                  minWidth: 200,
+                  height: 42,
+                  child: Text("Log In"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
